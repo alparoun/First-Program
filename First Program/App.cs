df833990db736b3f94555fb391172fcde201b1f1
MT <MT@DESKTOP-4VL3S6T>
2020-03-29 01:11:50 +0200

    Add project files.

---
+++ First Program/App.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace First_Program
+{
+    class App
+    {
+        static void Main(string[] args)
+        {
+
+            int @myvalue = 11 ;
+            Console.WriteLine(myvalue);
+            Console.Write("my dad");
+            Console.ReadKey();
+              
+
+
+        }
+        
+
+    }
+}
+
